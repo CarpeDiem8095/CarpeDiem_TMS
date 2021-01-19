ec95eb66b7a2deef41268787739575d04e057cd9
USER <USER@Seokgyu>
2021-01-19 16:51:26 +0900

    2021-01-19 조석규 회원가입 정규화 ajax처리 회원가입 구현 시큐리티 적용 진행중

--- CarpeDiem_TMS/src/main/java/com/cp/tms/model/member/MemberDaoImpl.java
+++ CarpeDiem_TMS/src/main/java/com/cp/tms/model/member/MemberDaoImpl.java
@@ -23,7 +23,7 @@ public class MemberDaoImpl implements IMemberDao {
 
 	@Override
 	public boolean singupMember(Member dto) {
-		int cnt = SqlSession.insert(CP+"signupMember",dto);
+		int cnt = SqlSession.insert(CP+"memberList",dto);
 		return (cnt>0)?true:false;
 	}
 	
@@ -42,12 +42,6 @@ public class MemberDaoImpl implements IMemberDao {
 		session.invalidate();
 	}
 
-
-
-	@Override
-	public int updatePw(Member vo) throws Exception {
-		return SqlSession.update(CP+"resetPassword", vo);
-	}
 
 
 
@@ -80,6 +74,34 @@ public class MemberDaoImpl implements IMemberDao {
 			return SqlSession.selectOne(CP+"EMDuplicateCheck", userEmail);
 		}
 
+
+		//이메일 발송
+		@Override
+		public void sendEmail(Member vo, String dto) {
+			
+			
+		}
+
+
+		//비밀번호 찾기
+		@Override
+		public void findPw(HttpServletResponse resp, Member vo) {
+			
+			
+		}
+
+
+
+		@Override
+		public int updatePw(Member vo) throws Exception {
+			
+			return SqlSession.update(CP+"resetPassword", vo);
+		}
+
+
+
+		
+	
 
 
 	
